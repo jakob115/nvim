ecfe168914dcfd404d4603f15a0c18dcef096898
jakob115 <123478185+jakob115@users.noreply.github.com>
2026-04-05 18:12:04 +0200

    added different signs for gitsigns

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -3,14 +3,42 @@ return {
 		"lewis6991/gitsigns.nvim",
 		opts = {
 			-- Default ones
+			-- signs = {
+			-- 	add = { text = "┃" },
+			-- 	change = { text = "┃" },
+			-- 	delete = { text = "_" },
+			-- 	topdelete = { text = "‾" },
+			-- 	changedelete = { text = "~" },
+			-- 	untracked = { text = "┆" },
+			-- },
+
+			-- signs = {
+			-- 	add = { text = "▐" },
+			-- 	change = { text = "▐" },
+			-- 	delete = { text = "▐" },
+			-- 	topdelete = { text = "▐" },
+			-- 	changedelete = { text = "▐" },
+			-- 	untracked = { text = "▐" },
+			-- },
+			--
+
 			signs = {
-				add = { text = "┃" },
-				change = { text = "┃" },
+				add = { text = "+" },
+				change = { text = "~" },
 				delete = { text = "_" },
 				topdelete = { text = "‾" },
 				changedelete = { text = "~" },
 				untracked = { text = "┆" },
 			},
+
+			-- signs = {
+			-- 	add = { text = "▎" },
+			-- 	change = { text = "▎" },
+			-- 	delete = { text = "" },
+			-- 	topdelete = { text = "" },
+			-- 	changedelete = { text = "▎" },
+			-- 	untracked = { text = "▎" },
+			-- },
 		},
 	},
 }
